f70625822e3c77e75c5ec6da9551f530292411f7
Daniel Vidal <vidalffyt@gmail.com>
2024-09-18 16:43:08 -0300

    add calendario

--- src/js/reservas.js
+++ src/js/reservas.js
@@ -1,4 +1,3 @@
-import { mostrarNotificacao } from './alerts.js';
 import { } from './firebase_config.js';
 
 function getEmail() {
@@ -14,160 +13,126 @@ function getEmail() {
         });
     });
 }
+    document.addEventListener('DOMContentLoaded', async function () {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            locale: 'pt-br',
+            initialView: 'dayGridMonth',
+            customButtons: {
+                addEventButton: {
+                    text: 'Adicionar evento',
+                    click: function () {
+                        modal.style.display = "block";
+                    }
+                }
+            },
+            eventClick: function (info) {
+                info.jsEvent.preventDefault();
 
-async function getCondominio() {
-    try {
-        const userEmail = await getEmail();
-        const userDb = firebase.firestore().collection('condominio');
-        const sindico = await userDb.where('email', '==', userEmail).get();
-        const sindicoData = sindico.docs[0].data();
-        const cod_condominio = sindicoData.cod_Condominio;
-        return cod_condominio;
-    } catch (error) {
-        return null;
-    }
-}
+                var modal = document.getElementById("eventModal");
+                modal.style.display = "block";
 
-// data inicio e data fim
+                // Defina o título do evento
+                document.getElementById("eventTitle").textContent = info.event.title;
 
+                document.getElementById("eventDetails").innerHTML = `
+                <div class="event-details">
+                    <p><strong>Morador:</strong> ${info.event.extendedProps.morador || "Não especificado"}</p>
+                    <p><strong>Status:</strong> ${info.event.extendedProps.status || "Não especificado"}</p>
+                    <p><strong>Local:</strong> ${info.event.extendedProps.local || "Não especificado"}</p>
+                    <p><strong>Tipo:</strong> ${info.event.extendedProps.tipo || "Não especificado"}</p>
+                    <p><strong>Início:</strong> ${info.event.start ? info.event.start.toLocaleString() : "Não especificado"}</p>
+                    <p><strong>Fim:</strong> ${info.event.end ? info.event.end.toLocaleString() : "Não especificado"}</p>
+                </div>
+            `;
 
-let [startDate, endDate] = [,];
+                var span = document.getElementsByClassName("close")[0];
+                span.onclick = function () {
+                    modal.style.display = "none";
+                };
 
-const datePicker = flatpickr("#date-picker", {
-    mode: "range",
-    dateFormat: "d/m/Y",
-    onChange: function(selectedDates) {
-        [startDate, endDate] = selectedDates;
-        if (startDate && endDate) {
-            startDate.setHours(0, 0, 0, 0);
-            endDate.setHours(23, 59, 59, 999);
-            let startTimestamp = startDate.getTime();
-            let endTimestamp = endDate.getTime();
-            
-            console.log("Start Timestamp:", startTimestamp);
-            console.log("End Timestamp:", endTimestamp);
+                window.onclick = function (event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                };
+            },
+            fixedWeekCount: false,
+            headerToolbar: {
+                left: 'prev,next',
+                right: 'today',
+                center: 'title'
+            },
+            dayMaxEventRows: 2,
+            buttonText: {
+                today: 'Hoje',
+            },
+            events: function (fetchInfo, successCallback, failureCallback) {
+                fetchEvents(fetchInfo, successCallback, failureCallback);
+            },
+            eventContent: function (arg) {
+                return {
+                    html: `
+                    <div class="event-title">${arg.event.title}</div>
+                `
+                };
+            }
+        });
 
-            filterPoints(startTimestamp, endTimestamp);
-        } else {
-            console.error("Start date or end date is not selected.");
+        calendar.render();
+
+        async function fetchEvents(fetchInfo, successCallback, failureCallback) {
+            const userEmail = await getEmail();
+            const userDb = firebase.firestore().collection('condominio');
+            const sindico = await userDb.where('email', '==', userEmail).get();
+            const sindicoData = sindico.docs[0].data();
+            const codigoSindico = sindicoData.cod_Condominio;
+
+            firebase.firestore().collection("eventos-manutencao")
+                .where("cod_condominio", "==", codigoSindico)
+                .onSnapshot(snapshot => {
+                    const eventos = snapshot.docs
+                        .map(doc => {
+                            const data = doc.data();
+                            const status = data.status;
+
+                            if (status === 'Cancelado') {
+                                return null;
+                            }
+
+                            return {
+                                id: doc.id,
+                                title: data.titulo,
+                                start: data.data_inicio.toDate(),
+                                end: data.data_fim.toDate(),
+                                classNames: [getClassByStatus(status)],
+                                extendedProps: {
+                                    tipo: data.tipo,
+                                    local: data.local,
+                                    status: data.status,
+                                    morador: data.morador,
+                                    valor: 'R$ ' + (data.valor / 100).toFixed(2).replace('.', ',')
+                                }
+                            };
+                        })
+                        .filter(event => event !== null); // Remove eventos cancelados
+
+                    successCallback(eventos);
+                });
         }
-    }
-});
 
-// Cards
-const cod_cond = await getCondominio();
-firebase.firestore().collection("eventos-manutencao")
-    .where('tipo', '==', 'evento')  
-    .orderBy('data_inicio', 'desc') 
-    .onSnapshot((snapshot) => {
-        snapshot.docChanges().forEach((change) => {
-            if (change.type === "added" && change.doc.data().status == 'Ativo' && change.doc.data().cod_condominio == cod_cond) {
-                createCard(change.doc.data());
+        function getClassByStatus(status) {
+            switch (status) {
+                case 'Reservado':
+                    return 'fc-event-green';
+                case 'Aguardando pagamento':
+                    return 'fc-event-orange';
+                case 'Manutenção':
+                    return 'fc-event-grey';
+                case 'Evento':
+                    return 'fc-event-red';
+                default:
+                    return 'fc-event-grey';
             }
-            if (change.type === "modified") {
-                console.log("Documento modificado: ", change.doc.data());
-                createCard(null); 
-            }
-            if (change.type === "removed") {
-                console.log("Documento removido: ", change.doc.data());
-                createCard(null); 
-            }
-        });
+        }
     });
-async function createCard(doc) {
-    
-    const ticket_ul = document.querySelector('#tickets-ativos-ul');
-    const ticket = document.createElement('li');
-    ticket.setAttribute('class', 'ticket');
-
-    ticket.innerHTML = `
-                <div class="ticket-header">
-                    <p class="ticket-id">Local: <span>${doc.local}</span></p>
-                    <p class="ticket-status">Status: <span>${doc.status}</span></p>
-                </div>
-                <div class="ticket-body">
-                    <p>Nome do morador: <span class="ticket-description">${doc.morador}</span></p>
-                    <p>Tipo: <span class="ticket-description">Evento</span></p>
-                </div>
-                <div class="ticket-footer">
-                    <span class="ticket-creation-date" style="font-weight:bold">Inicio da reserva: <br>${convertTimestampToDate(doc.data_inicio)}</span>
-                    <span class="ticket-creation-date" style="font-weight:bold">Fim da reserva:  <br>${convertTimestampToDate(doc.data_fim)}</span>
-                </div>   
-    `;
-    ticket_ul.appendChild(ticket);
- 
-}
-
-function convertTimestampToDate(timestamp) {
-    
-    console.log("NOVO: " + timestamp)
-    const date = new Date(timestamp.seconds * 1000);
-
-    
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0'); 
-    const year = date.getFullYear();
-
-   
-    return `${day}/${month}/${year}`;
-}
-
-async function filterPoints(startTimestamp, endTimestamp) {
-    const ticket_ul = document.querySelector('#tickets-ativos-ul');
-    ticket_ul.innerHTML = '';
-
-    const eventosDb = firebase.firestore().collection('eventos-manutencao');
-
-    const eventos = await eventosDb
-        .where('data_inicio', '>=', new firebase.firestore.Timestamp(startTimestamp / 1000, 0))
-        .where('data_inicio', '<=', new firebase.firestore.Timestamp(endTimestamp / 1000, 0))
-        .where('status','==','Ativo')
-        .where('cod_condominio','==',cod_cond)
-        .get();
-
-    if (!eventos.empty) {
-        const docs = eventos.docs;
-
-        docs.forEach(doc => {
-            createCard(doc.data());
-        });
-    } else {
-        mostrarNotificacao('error', 'Não tem reservas nesse período', 'Reservas');
-        loadAllCards();
-    }
-}
-const clearDatesButton = document.getElementById("clear-dates");
-clearDatesButton.addEventListener('click',clearDates);
-
-function clearDates() {
-
-    if (datePicker) {
-        datePicker.clear();
-    }
-
-    const ticket_ul = document.querySelector('#tickets-ativos-ul');
-    if (ticket_ul) {
-        ticket_ul.innerHTML = ''; 
-    }
-
-    loadAllCards(); 
-}
-
-async function loadAllCards() {
-    const eventosDb = firebase.firestore().collection('eventos-manutencao');
-
-    const eventos = await eventosDb
-        .where('status', '==', 'Ativo')
-        .where('cod_condominio', '==', cod_cond)
-        .get();
-
-    if (!eventos.empty) {
-        const docs = eventos.docs;
-
-        docs.forEach(doc => {
-            createCard(doc.data());
-        });
-    } else {
-        mostrarNotificacao('error', 'Não tem reservas nesse período', 'Reservas');
-    }
-}
